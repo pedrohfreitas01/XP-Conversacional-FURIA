dc7163497fa5663ac54358bd652149c3f2687911
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-05-04 23:46:31 -0300

    fix: vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './Components/Header/header'
+import Header from './Components/Header/Header'
 import Main from './Components/Main/Main'
 
 function App() {
